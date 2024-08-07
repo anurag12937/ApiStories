5f018423ae6aa6b3c9b0eac4269de9a7dbf9ac24
anurag12937 <anurag.rai@rsystems.com>
2024-08-07 11:25:10 +0530

    Add files via upload
    initial commit

---
+++ CodingChallenge/Configurations/MapperProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using CodingChallenge.Core.Models;
+using System.Diagnostics.CodeAnalysis;
+
+namespace CodingChallenge.Configurations
+{
+    [ExcludeFromCodeCoverage]
+    public class MapperProfile : Profile
+    {
+
+        public MapperProfile()
+        {
+            CreateMap<StoryDetail, StoryDetailDto>().ReverseMap();
+        }
+    }
+}
